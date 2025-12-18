db49a8691d2334fa05e67791b0452d8c731ff7d4
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2025-12-18 22:58:33 +0630

    Second Step

--- databaseconfig.py
+++ databaseconfig.py
@@ -8,7 +8,14 @@ load_dotenv()
 mongo_uri = os.getenv("MONGO_URI","mongodb://localhost:27017/")
 
 #Connect to MongoDB server
-client = MongoClient(mongo_uri)
+# client = MongoClient(mongo_uri)
+client = MongoClient(
+     mongo_uri,
+     tls=True,
+     serverSelectionTimeoutMS=20000,
+     connectTimeoutMS=20000
+)
+client.admin.command("ping")
 
 # Select database and collection
 db = client["mydatabase"]
